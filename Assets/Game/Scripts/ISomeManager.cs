1c7b51ba135b6c8c962695883cb7dd5d68809d7b
deepwaterooo <heyanh@vandals.uidaho.edu>
2018-04-27 15:28:42 -0700

    game flow processing ...

--- Assets/Game/Scripts/ISomeManager.cs
+++
@@ -1,3 +0,0 @@
-﻿public interface ISomeManager {
-    void DoManagement();
-}
--- Assets/Scripts/SlotsContext.cs
+++ Assets/Scripts/SlotsContext.cs
@@ -13,25 +13,20 @@ public class SlotsContext : MVCSContext {
     protected override void mapBindings() {
         base.mapBindings();
 
-
-        injectionBinder.Bind<IScore>().To<ScoreModel>().ToSingleton();    // 单例模式
-        injectionBinder.Bind<ICredit>().To<CreditModel>().ToSingleton();  // 单例模式
-        
+        injectionBinder.Bind<IScore>().To<ScoreModel>().ToSingleton();    
+        injectionBinder.Bind<ICredit>().To<CreditModel>().ToSingleton();  
         injectionBinder.Bind<StartSpin>().ToSingleton();               
         injectionBinder.Bind<StopSpin>().ToSingleton();                
 
-        mediationBinder.Bind<LeverView>().To<LeverMediator>();         // 中介者模式
+        mediationBinder.Bind<LeverView>().To<LeverMediator>();            
         mediationBinder.Bind<SlotView>().To<SlotMediator>();
         mediationBinder.Bind<ScoreTextView>().To<ScoreTextMediator>(); 
         mediationBinder.Bind<CreditTextView>().To<CreditTextMediator>(); 
         mediationBinder.Bind<SlotObjectView>().To<SlotObjectMediator>();
-        
         mediationBinder.Bind<MainUIView>().To<MainUIViewMediator>();
 
-        //commandBinder.Bind<AddScoreSignal>().To<ChangeScoreCommand>().Pooled; // should not work for control
-        commandBinder.Bind(Events.CHANGE_SCORE).To<ChangeScoreCommand>();
+        commandBinder.Bind(Events.CHANGE_SCORE).InSequence().To<ChangeScoreCommand>(); //.To<ChangeCreditCommand>();
         commandBinder.Bind(Events.CHANGE_CREDIT).To<ChangeCreditCommand>();
-
         commandBinder.Bind(NotificationCenter.SPIN).To<SpinCommand>();
         commandBinder.Bind(NotificationCenter.VOLUMN).To<VolumnCommand>();        
     }
